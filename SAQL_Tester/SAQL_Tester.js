c4398c5586839fcb2d53ffad509e874ab4683625
Marc Hassan <marc.j.hassan@gmail.com>
2014-12-17 10:17:34 -0500

    Update SAQL_Tester.js

--- SAQL_Tester/SAQL_Tester.js
+++ SAQL_Tester/SAQL_Tester.js
@@ -15,64 +15,65 @@ STMyJS = document.createElement("script");
 STMyJS.id="STMyJS";
 STMyJS.type = "text/javascript";
 STMyJS.innerHTML=' \
-	  function STCleanUp() { \
-		  document.getElementById("STMyDiv").style.display="none"; \
-		  } \
-      function STBuildQuery() { \
-	      return JSON.stringify({"action":"query","query":document.getElementById("STMyInput").value.replace(/\\\\\\"/g,"\\"")}); \
-		  } \
-      function STSendQuery() { \
-	  var STDataString = STBuildQuery(); \
-        $.ajax({ \
-          url: window.location.protocol+\'//\'+window.location.hostname+\'/insights/internal_api/v1.0/remote\', \
-          type: \'POST\', \
-		  data: STDataString, \
-          dataType: \'json\', \
-          success: function(resp) { \
-			var STMyRecords = JSON.parse(resp.result[0]).results.records; \
-			var STMyCols = Object.keys(STMyRecords[0]); \
-			var STMyNumCols = STMyCols.length; \
-			var STMyNumRows = STMyRecords.length; \
-			var STMyHTML = ""; \
-			STMyHTML+=\'<table style="margin:5px;border-collapse:separate">\'; \
-			/* Header row */ \
-			STMyHTML+=\'<tr>\'; \
-			for (i=0;i<STMyNumCols;i++) {STMyHTML+=\'<th style="border:1px solid;">\'+Object.keys(STMyRecords[0])[i]+\'</th>\';} \
-			STMyHTML+=\'</tr>\'; \
-			/* Data rows */ \
-			for (j=0;j<STMyNumRows;j++) { \
-			STMyHTML+=\'<tr>\'; \
-			for (i=0;i<STMyNumCols;i++) {STMyHTML+=\'<td style="border:1px solid;">\'+STMyRecords[j][STMyCols[i]]+\'</td>\';} \
-			STMyHTML+=\'</tr>\'; \
-			} \
-			STMyHTML+=\'</table>\'; \
-			document.getElementById("STMyOutput").innerHTML=STMyHTML;  }, \
-          error: function(resp) { document.getElementById("STMyOutput").innerHTML=\'<div style="margin:5px;"><span style="color:red;">ERROR:</span> \'+JSON.parse(resp.responseText).errorMsg+\'</div>\'; }, \
-          beforeSend: STSetHeader \
-        }); \
+function STCleanUp() { \
+document.getElementById("STMyDiv").style.display="none"; \
+} \
+function STBuildQuery() { \
+  return JSON.stringify({"action":"query","query":document.getElementById("STMyInput").value.replace(/\\\\\\"/g,"\\"")}); \
+} \
+function STSendQuery() { \
+  var STDataString = STBuildQuery(); \
+  $.ajax({ \
+    url: window.location.protocol+\'//\'+window.location.hostname+\'/insights/internal_api/v1.0/remote\', \
+    type: \'POST\', \
+    data: STDataString, \
+    dataType: \'json\', \
+    success: function(resp) { \
+      var STMyRecords = JSON.parse(resp.result[0]).results.records; \
+      var STMyCols = Object.keys(STMyRecords[0]); \
+      var STMyNumCols = STMyCols.length; \
+      var STMyNumRows = STMyRecords.length; \
+      var STMyHTML = ""; \
+      STMyHTML+=\'<table style="margin:5px;border-collapse:separate">\'; \
+      /* Header row */ \
+      STMyHTML+=\'<tr>\'; \
+      for (i=0;i<STMyNumCols;i++) {STMyHTML+=\'<th style="border:1px solid;">\'+Object.keys(STMyRecords[0])[i]+\'</th>\';} \
+      STMyHTML+=\'</tr>\'; \
+      /* Data rows */ \
+      for (j=0;j<STMyNumRows;j++) { \
+      STMyHTML+=\'<tr>\'; \
+      for (i=0;i<STMyNumCols;i++) {STMyHTML+=\'<td style="border:1px solid;">\'+STMyRecords[j][STMyCols[i]]+\'</td>\';} \
+      STMyHTML+=\'</tr>\'; \
       } \
-      function STSetHeader(xhr) { \
-		xhr.setRequestHeader(\'Host\',window.location.hostname); \
-		xhr.setRequestHeader(\'Accept\',\'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8\'); \
-		xhr.setRequestHeader(\'Accept-Language\',\'en-US,en;q=0.5\'); \
-		xhr.setRequestHeader(\'Authorization\',\'OAuth \'+STGetCookie("sid")); \
-		xhr.setRequestHeader(\'Referer\',window.location.protocol+\'//\'+window.location.hostname+\'/analytics/wave/wave.apexp\'); \
-		xhr.setRequestHeader(\'Content-Length\',STBuildQuery().length); \
-		xhr.setRequestHeader(\'Content-Type\',\'text/plain; charset=UTF-8\'); \
-		xhr.setRequestHeader(\'Connection\',\'keep-alive\'); \
-		xhr.setRequestHeader(\'Pragma\',\'no-cache\'); \
-		xhr.setRequestHeader(\'Cache-Control\',\'no-cache\'); \
-      } \
-	  function STGetCookie(cname) { \
-    var name = cname + "="; \
-    var ca = document.cookie.split(\';\'); \
-    for(var i=0; i<ca.length; i++) { \
-        var c = ca[i]; \
-        while (c.charAt(0)==\' \') c = c.substring(1); \
-        if (c.indexOf(name) != -1) return c.substring(name.length,c.length); \
-    } \
-    return ""; \
-}';
+      STMyHTML+=\'</table>\'; \
+      document.getElementById("STMyOutput").innerHTML=STMyHTML;  }, \
+    error: function(resp) { document.getElementById("STMyOutput").innerHTML=\'<div style="margin:5px;"><span style="color:red;">ERROR:</span> \'+JSON.parse(resp.responseText).errorMsg+\'</div>\'; }, \
+    beforeSend: STSetHeader \
+  }); \
+} \
+function STSetHeader(xhr) { \
+  xhr.setRequestHeader(\'Host\',window.location.hostname); \
+  xhr.setRequestHeader(\'Accept\',\'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8\'); \
+  xhr.setRequestHeader(\'Accept-Language\',\'en-US,en;q=0.5\'); \
+  xhr.setRequestHeader(\'Authorization\',\'OAuth \'+STGetCookie("sid")); \
+  xhr.setRequestHeader(\'Referer\',window.location.protocol+\'//\'+window.location.hostname+\'/analytics/wave/wave.apexp\'); \
+  xhr.setRequestHeader(\'Content-Length\',STBuildQuery().length); \
+  xhr.setRequestHeader(\'Content-Type\',\'text/plain; charset=UTF-8\'); \
+  xhr.setRequestHeader(\'Connection\',\'keep-alive\'); \
+  xhr.setRequestHeader(\'Pragma\',\'no-cache\'); \
+  xhr.setRequestHeader(\'Cache-Control\',\'no-cache\'); \
+} \
+function STGetCookie(cname) { \
+  var name = cname + "="; \
+  var ca = document.cookie.split(\';\'); \
+  for(var i=0; i<ca.length; i++) { \
+    var c = ca[i]; \
+    while (c.charAt(0)==\' \') c = c.substring(1); \
+    if (c.indexOf(name) != -1) return c.substring(name.length,c.length); \
+  } \
+  return ""; \
+} \
+';
 
 STMyDiv = document.createElement("div");
 STMyDiv.id = "STMyDiv";
